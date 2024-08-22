6243dfd07621a99d6ba2e4581797f9a5d13f92bb
Paweł <pawel.stach81@gmail.com>
2024-08-22 18:30:53 +0200

    updata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,29 +4,29 @@ import Hero from "@/components/Hero";
 import Serwice from "@/components/Serwice";
 import ParallaxTextOnX from "@/components/ui/ParallaxTextOnX";
 import { useEffect } from "react";
+import { ReactLenis, useLenis } from "lenis/react";
 
 export default function Home() {
-  useEffect(() => {
-    (async () => {
-      const LocomotiveScroll = (await import("locomotive-scroll")).default;
-      const locomotiveScroll = new LocomotiveScroll();
-    })();
-  }, []);
+  const lenis = useLenis(({ scroll }) => {
+    // called every scroll
+  });
   return (
     <main>
-      <Hero />
-      <div className="containerSection">
-        <AboutUs />
-        <Serwice />
-        <div className="py-16">
-          <ParallaxTextOnX baseVelocity={-1}>
-            Longines Baume&Mercier Patek Philippe tagHeurt
-          </ParallaxTextOnX>
-          <ParallaxTextOnX baseVelocity={1}>
-            Longines Baume&Mercier Patek Philippe tagHeurt
-          </ParallaxTextOnX>
+      <ReactLenis root>
+        <Hero />
+        <div className="containerSection">
+          <AboutUs />
+          <Serwice />
+          <div className="py-16">
+            <ParallaxTextOnX baseVelocity={-1}>
+              Longines Baume&Mercier Patek Philippe tagHeurt
+            </ParallaxTextOnX>
+            <ParallaxTextOnX baseVelocity={1}>
+              Longines Baume&Mercier Patek Philippe tagHeurt
+            </ParallaxTextOnX>
+          </div>
         </div>
-      </div>
+      </ReactLenis>
     </main>
   );
 }
--- src/components/Serwice.tsx
+++ src/components/Serwice.tsx
@@ -24,7 +24,7 @@ export default function Serwice() {
             return (
               <ClientGlsl
                 key={index}
-                className="w-full uppercase text-2xl md:text-[3vw] py-4 border-white border-b flex justify-start cursor-pointer"
+                className="w-full uppercase text-2xl md:text-[3vw] py-8 border-white border-b flex justify-start cursor-pointer"
               >
                 <h2>{project.title}</h2>
               </ClientGlsl>
